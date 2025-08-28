26159d5f6bc30e79fad20f59559d250856104ab1
Wouter Nuijten <wouternuijten@gmail.com>
2025-08-28 17:01:59 +0100

    Update job interface

--- src/interfaces/job.jl
+++ src/interfaces/job.jl
@@ -4,10 +4,10 @@ import Unitful: 𝐓
 abstract type Job end
 
 """
-    execute(job::Job, t::Quantity{<:Number, 𝐓})
+    progress!(job::Job, t::Quantity{<:Number, 𝐓})
 
 Execute the job with the current time. The job decides internally whether to perform work.
 """
 function progress!(job::Job, t::Quantity{<:Number, 𝐓})
-    throw(NotImplementedError("`execute` is not implemented for $(typeof(job))"))
+    throw(NotImplementedError("`progress!` is not implemented for $(typeof(job))"))
 end
