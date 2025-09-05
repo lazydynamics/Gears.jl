fcb2787b42ed5122292c0993f7ed569d1ed0c933
Wouter Nuijten <wouternuijten@gmail.com>
2025-09-05 10:46:26 +0200

    Make machine clock start at 0

--- src/implementations/clocks/machine.jl
+++ src/implementations/clocks/machine.jl
@@ -1,7 +1,16 @@
 using Unitful
 
-struct MachineClock <: Clock end
+"""
+    MachineClock
+
+A clock that uses the system time. The time is represented in seconds.
+"""
+struct MachineClock{T} <: Clock
+    start_time::T
+end
+
+MachineClock() = MachineClock(time()u"s")
 
 function now(clock::MachineClock)
-    return time()u"s"
+    return time()u"s" - clock.start_time
 end
--- test/implementations/clocks/machine_clock_tests.jl
+++ test/implementations/clocks/machine_clock_tests.jl
@@ -1,12 +1,38 @@
-@testitem "MachineClock" begin
+@testitem "MachineClock constructor" begin
+    import EnvironmentEngine: MachineClock, now
+    using Unitful
+
+    # Test default constructor
+    clock = MachineClock()
+    @test unit(clock.start_time) == u"s"
+
+    clock = MachineClock()
+    initial_time = now(clock)
+
+    # The clock should start very close to 0 (allowing for small execution time)
+    @test @inferred initial_time >= 0u"s"
+    @test @inferred initial_time < 0.01u"s"  # Should be less than 10ms
+end
+
+@testitem "MachineClock time progression" begin
     import EnvironmentEngine: MachineClock, now
     using Unitful
 
     clock = MachineClock()
 
-    @test now(clock) > 0u"s"
+    # Test that time increases monotonically
+    times = [now(clock) for _ in 1:5]
+    for i in 2:length(times)
+        @test times[i] >= times[i - 1]
+    end
+
+    # Test that sleep duration is reflected in time difference
     before = now(clock)
     sleep(0.1)
     after = now(clock)
-    @test after > before
+    time_diff = after - before
+
+    @test time_diff > 0u"s"
+    @test time_diff >= 0.09u"s"  # Allow for some timing precision issues
+    @test time_diff <= 0.2u"s"   # Should not be too much more than sleep time
 end
