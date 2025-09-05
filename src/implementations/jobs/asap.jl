76af6a97339bd773cbda04a6d056082b03efc911
Wouter Nuijten <wouternuijten@gmail.com>
2025-09-05 10:51:31 +0200

    Refine testing of `AsapJob`

--- src/implementations/jobs/asap.jl
+++ src/implementations/jobs/asap.jl
@@ -5,7 +5,7 @@ struct AsapJob{F} <: Job
     is_processing::Atomic{Bool}
 end
 
-function progress!(job::AsapJob)
+function progress!(job::AsapJob, dt::Quantity{<:Number, 𝐓})
     if !atomic_or!(job.is_processing, true)
         job.f()
         atomic_xchg!(job.is_processing, false)
--- test/implementations/jobs/asapjob_tests.jl
+++ test/implementations/jobs/asapjob_tests.jl
@@ -17,10 +17,30 @@ end
     end
 
     job = AsapJob(f)
-    progress!(job)
+    progress!(job, 1ms)
 
     @test num_calls == 1
-    progress!(job)
+    progress!(job, 100s)
 
     @test num_calls == 2
+end
+
+@testitem "Test multithreaded" setup = [MockSchedulers, MockClocks] begin
+    import EnvironmentEngine: AsapJob, progress!
+    n_threads = Base.Threads.nthreads()
+    if n_threads > 1
+        counter = 0
+        function f()
+            sleep(0.1)
+            global counter += 1
+        end
+        job = AsapJob(f)
+        Threads.@threads for i in 1:n_threads
+            progress!(job, 1ms)
+        end
+        @test counter == 1
+    else
+        has_multiple_threads = false
+        @test_broken has_multiple_threads
+    end
 end
