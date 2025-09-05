c6c823f45a83a6e63c7d3a33c13090bf56dbbfa4
Wouter Nuijten <wouternuijten@gmail.com>
2025-09-05 15:30:37 +0200

    Update docs

--- docs/make.jl
+++ docs/make.jl
@@ -10,15 +10,7 @@ makedocs(;
     format = Documenter.HTML(;
         canonical = "https://lazydynamics.github.io/EnvironmentEngine.jl", edit_link = "main", assets = String[]
     ),
-    pages = [
-        "Home" => "index.md",
-        "Getting Started" => "getting_started.md",
-        "API Specifications" => [
-            "Drone Environment" => "api_spec/drone.md",
-            "Farama Gym Environment" => "api_spec/farama.md",
-            "Multi-Agent Environment" => "api_spec/multi_agent.md"
-        ]
-    ]
+    pages = ["Home" => "index.md", "Getting Started" => "getting_started.md"]
 )
 
 deploydocs(; repo = "github.com/lazydynamics/EnvironmentEngine.jl", devbranch = "main")
